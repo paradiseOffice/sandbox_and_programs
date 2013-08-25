523c90abd47d848d203b7b8fdfa9d34d1c74e17d
Hazel Windle <lead-dev@linux-paradise.co.uk>
2013-08-25 16:04:04 +0100

    Fixed the crappy menus (almost)

--- dict_shelve_end.py
+++
@@ -1,3 +0,0 @@
-if __name__ == '__main__':	# When run as a script
-  for key in db:
-    print(key, '=>\n	', db[key])
--- paradise_office_site/sandbox_v1.0/old_demo/scripts/menu-slider.js
+++ paradise_office_site/sandbox_v1.0/old_demo/scripts/menu-slider.js
@@ -1,18 +1,135 @@
 /* For animating CSS drop down menus */
-$('body').ready(function() {
-    $('.menuHead').hover ( function() {
-      $('.menuBody')
+$(document).ready(function() {
+   
+    $('#menuLink1').hover ( function() {
+      $('#menu1')
         .stop(true, true)
-        .delay(200)
-        .show(800)
+        .delay(10)
+        .show(200)
         .delay(5000)
-        .hide(600);
-      }); /* showing the sub menus, which all have a class of menuBody */
+        .hide(200);
+      });
     
       $(document).click ( function() {
-          $('.menuBody').hide(); /* hiding the menus when the user clicks anywhere else */
+         $('#menu1').hide(); 
+      });
+      
+      $('#menuLink2').hover ( function() {
+      $('#menu2')
+        .stop(true, true)
+        .delay(10)
+        .show(200)
+        .delay(5000)
+        .hide(200);
+      });
+    
+      $(document).click ( function() {
+         $('#menu2').hide(); 
       });
 
-
+      $('#menuLink3').hover ( function() {
+      $('#menu3')
+        .stop(true, true)
+        .delay(10)
+        .show(200)
+        .delay(5000)
+        .hide(200);
+      });
+    
+      $(document).click ( function() {
+         $('#menu3').hide(); 
+      });
+      
+      $('#menuLink4').hover ( function() {
+      $('#menu4')
+        .stop(true, true)
+        .delay(10)
+        .show(200)
+        .delay(5000)
+        .hide(200);
+      });
+    
+      $(document).click ( function() {
+         $('#menu4').hide(); 
+      });
+      
+      $('#menuLink5').hover ( function() {
+      $('#menu5')
+        .stop(true, true)
+        .delay(10)
+        .show(200)
+        .delay(5000)
+        .hide(200);
+      });
+    
+      $(document).click ( function() {
+         $('#menu5').hide(); 
+      });
+      
+      $('#menuLink6').hover ( function() {
+      $('#menu6')
+        .stop(true, true)
+        .delay(10)
+        .show(200)
+        .delay(5000)
+        .hide(200);
+      });
+    
+      $(document).click ( function() {
+         $('#menu6').hide(); 
+      });
+      
+      $('#menuLink7').hover ( function() {
+      $('#menu7')
+        .stop(true, true)
+        .delay(10)
+        .show(200)
+        .delay(5000)
+        .hide(200);
+      });
+    
+      $(document).click ( function() {
+         $('#menu7').hide(); 
+      });
+      
+      $('#menuLink8').hover ( function() {
+      $('#menu8')
+        .stop(true, true)
+        .delay(10)
+        .show(200)
+        .delay(5000)
+        .hide(200);
+      });
+    
+      $(document).click ( function() {
+         $('#menu8').hide(); 
+      });
+      
+      $('#menuLink9').hover ( function() {
+      $('#menu9')
+        .stop(true, true)
+        .delay(10)
+        .show(200)
+        .delay(5000)
+        .hide(200);
+      });
+    
+      $(document).click ( function() {
+         $('#menu9').hide(); 
+      });
+      
+      $('#menuLink10').hover ( function() {
+      $('#menu10')
+        .stop(true, true)
+        .delay(10)
+        .show(200)
+        .delay(5000)
+        .hide(200);
+      });
+    
+      $(document).click ( function() {
+         $('#menu10').hide(); 
+      });
+      
 });
   
