d8e8a36a728d44e8a1c10df4d166a3bc947b8e20
Hazel Windle <lead-dev@linux-paradise.co.uk>
2013-11-05 14:26:09 +0000

    file mimetype reader written

--- sandbox/sandbox/logic/file_port/doc_conv_upload.py
+++ sandbox/sandbox/logic/file_port/doc_conv_upload.py
@@ -1,60 +1,21 @@
 #!/usr/bin/python3.3
 
-import os
+import os, sys, mimetypes
 
 def doc_to_odt(file):
     filename = file
-    pid = os.fork()
+    # subprocess..
     folder = "msoffice_files/"
     infile = folder + filename # os.path.join
     # if not dir os.path.exists : mkdir
-    commandargs = ['--headless', '--invisible', '--nologo', '--convert-to odt', '--outdir ./ ' ]
-    commandargs.append(infile)
-    args = tuple(commandargs)
-    print(args[:])
-    os.execl('libreoffice', '--headless --invisible --nologo --convert-to odt --outdir ./ ', str(infile)  ) # buggy, see below.
+    #['--headless', '--invisible', '--nologo', '--convert-to odt', '--outdir ./ ' ]
+    #commandargs.append(infile)
+    #args = tuple(commandargs)
+    #print(args[:])
+    #os.execl('libreoffice', '--headless --invisible --nologo --convert-to odt --outdir ./ ', str(infile)  ) # buggy, see below.
     # This works on Unix like platforms only, for windows it's spawn
 
 """
-"starts programs until you type 'q'"
-import os
-parm = 0
-while True:
-  parm += 1
-  pid = os.fork()
-  if pid == 0:
-  # copy process
-    os.execl('ython', 'python', 'child.py', str(parm)) # overlay program
-    assert False, 'error starting program'
-  # shouldn't return
-  else:
-    print('Child is', pid)
-    if input() == 'q': break
-
-
-os.execv(program, commandlinesequence)
-The basic “v” exec form is passed an executable program’s name, along with a list
-or tuple of command-line argument strings used to run the executable (that is, the
-words you would normally type in a shell to start a program).
-os.execl(program, cmdarg1, cmdarg2,... cmdargN)
-The basic “l” exec form is passed an executable’s name, followed by one or more
-command-line arguments passed as individual function arguments. This is the
-same as os.execv(program, (cmdarg1, cmdarg2,...)).
-os.execlp
-os.execvp
-Adding the letter p to the execv and execl names means that Python will locate the
-executable’s directory using your system search-path setting (i.e., PATH).
-os.execle
-os.execve
-Adding a letter e to the execv and execl names means an extra, last argument is a
-dictionary containing shell environment variables to send to the program.
-os.execvpe
-os.execlpe
-Adding the letters p and e to the basic exec names means to use the search path
-and to accept a shell environment settings dictionary.
-
-"""
-
 def dot_to_ott(file):
     filename = file # check this is a dot file
     pid = os.fork()
@@ -212,6 +173,7 @@ def pdf_to_html(file):
     args = tuple(commandargs)
     print(args[:])
     os.execl('libreoffice', '--headless --invisible --nologo --convert-to html --outdir ./ ', str(infile)  ) # buggy, see below.
+"""
 
 if __name__ == '__main__':
     doc_to_odt('ecdl1.doc')
--- sandbox/sandbox/logic/file_port/file_mimetype_reader.py
+++ sandbox/sandbox/logic/file_port/file_mimetype_reader.py
@@ -3,18 +3,18 @@
 import os, sys, mimetypes
 
 def file_tester():
+    i = 1
     testfile = sys.argv[1]
     print(testfile)
     if not os.path.exists(testfile): print("File doesn't exist")
     contenttype, encoding = mimetypes.guess_type(testfile)
     if contenttype == None or encoding is not None:
         contenttype = '?/?'
-    maintype, subtype = contenttype.split('/', 1)
-    print(maintype)
-    print('/' + subtype)
-    #if maintype == 'text' and subtype == 'html':
-    #    return
-    #else:
-    #    raise WrongFileError("That's not an HTML file!" )
+
+    fileinfo = open('file' + str(i), 'w')
+    fileinfo.write(testfile + "\n")
+    fileinfo.write(contenttype)
+    fileinfo.close()
+
 
 if __name__ == '__main__': file_tester()
