46a497f1f7358098f57b6aa880618f379d286aba
Hazel Windle <lead-dev@linux-paradise.co.uk>
2014-03-16 20:56:51 +0000

    Wrote some small Javascript functions

--- features/sandbox/logic/file_port/open/new.js
+++ features/sandbox/logic/file_port/open/new.js
@@ -1,4 +1,6 @@
- $(function() {
+/* Initialises tabs, goes in other utilities Javascript function. */
+
+$(function() {
     $("#tabrow")
       .tabs()
       .find( ".ui-tabs-nav" )
@@ -6,15 +8,63 @@
     $( ".tabs-top .ui-tabs-nav, .tabs-top .ui-tabs-nav > * " )
       .removeClass( "ui-corner-all ui-corner-bottom" )
       .addClass( "ui-corner-top" );
-    }); 
+}); 
 
-/* #new_file */
- $(document).ready(function() {
-   var tabs = $( "#tabrow" ).tabs();
-   $( "#new_file" ).click(function() {
-     var num_open_files = $( "#tabrow ul li .open" ).length();
-     var next_file_num = num_open_files + 1;
-     $( "#tabrow #tab_" + next_file_num ).addClass(".open");
-   }); 
-});
+
+/* #new_file - adds the default classes to tab_n */
+function newFile () {
+  $(document).ready(function() {
+    $( "#new_file" ).click(function() {
+      var numOpenTabs = $( "#tabrow ul li .open" ).length();
+      var nextTabNum = parseInt(numOpenTabs) + 1;
+      $( "#tabrow #tab_" + nextTabNum )
+      	.addClass(".open")
+	.removeClass(".closed");
+      // Emptying the file of content.
+      $( "#file" + nextTabNum + "_wrapper" ).html("");
+    }); // end of click function 
+  });  //end of inner function
+};
+
+/* close a file */
+function closeFile () {
+  $(function() {
+    $( "#close_file").click(function() {
+      var currentTab = findCurrentTab();
+      $(currentTab).removeClass(".open")
+        .addClass(".closed");
+    });
+  });
+};
+
+/* to go into doc_utilities along with other functions from here */
+function findCurrentTab() {
+    var tab = $( "#tabrow" )
+    // check line below
+    .find( ".ui-tab-selected" );
+    tab.css({ color: #f00; }); // DEBUG
+    return tab;
+};
+
+function getTabFName () {
+    var selectedTab = findCurrentTab();
+    var fileName = $(selectedTab + "[data-filename]" ).attr("data-filename");
+    console.log(fileName); // DEBUG
+    return fileName;
+};
+
+function setTabFName (filename) {
+    // filename sent via ajax
+    var selectedTab = findCurrentTab();
+    $(selectedTab).attr("data-filename", "filename"); // check
+    console.log(filename); // DEBUG, testing AJAX
+    return true;
+};
+
+/* should go somewhere else */
+function fillTab (file) {
+    var tab = findCurrentTab();
+    $(tab).html(file);
+    return true;
+};
 
