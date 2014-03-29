a126117366cc56350c4f94bc1d8e87815f74a4d8
Hazel Windle <lead-dev@linux-paradise.co.uk>
2014-03-29 19:16:09 +0000

    Tidying up monster CSS files

--- js_lib/resize-screen.js
+++ js_lib/resize-screen.js
@@ -1,48 +1,51 @@
- /* get the document areas height and width (viewport, using jQuery.
- 
-  var toolbarHeight = height * 0.30 ( work out total percentage / 100)
- add size choosing script function here too, with the value passed
- as a parameter to the resizing function 
- resize any pixel values if needed. 
-*/
-
-
+/* 
+ * This script puts the status bar on the bottom, after the file port and makes the
+ * file port fill the windows space dynamically, and on page refresh. 
+ */
 var checkWidth = function() {
-  // innerWidth, innerHeight works when page is zoomed.
-  var viewportWidth = jQuery(window).innerWidth();
-  var viewportHeight = jQuery(window).innerHeight();
-  var titlebar = jQuery('.title_bar-skel').height();
-  var menubar = jQuery('.menu_bar-skel').height();
-  var commonbar = jQuery('.common_bar-skel').outerHeight(true);
-  var statusbar = jQuery('.status_bar').outerHeight(true);
+  // Get the window dimensions, save to variables.
+  var viewportWidth = window.innerWidth;
+  // console.log("viewportWidth: " + viewportWidth); // DEBUG
+  var viewportHeight = window.innerHeight;
+  // console.log("viewportHeight: " + viewportHeight); // DEBUG
+  // Getting the margins as well with outerHeight(true)
+  var titlebar = $('.title_bar-skel').outerHeight(true);
+  var menubar = $('.menu_bar-skel').outerHeight(true);
+  var commonbar = $('.common_bar-skel').outerHeight(true);
+  var statusbar = $('.status_bar').outerHeight(true);
+  // console.log("titlebar: " + titlebar + " menu bar: " + menubar + " common bar: " + commonbar + " status bar: " + statusbar); // DEBUG
   
-  var toolPane = jQuery('.tool_pane-skel').outerWidth(true);
+  var toolPane = $('.tool_pane-skel').outerWidth(true);
   var toolbarHeight = parseInt(titlebar + menubar + commonbar + statusbar); 
-  console.log("height of all toolbars: " + toolbarHeight);
-  console.log("Width of tool panes: " + toolPane);
+  // console.log("height of all toolbars: " + toolbarHeight);
+  // console.log("Width of tool panes: " + toolPane);
   
-  // toolbarHeight = parseInt(toolbarHeight);
-  toolPane = parseInt(toolPane) + 30; // For borders and 15px left border
   viewportHeight = viewportHeight - toolbarHeight;
   viewportHeight = parseInt(viewportHeight);  
-  fileportWidth = viewportWidth - toolPane ;
+  // The 15 is the from left border 
+  var leftPos = 15;
+  fileportWidth = viewportWidth - (toolPane + leftPos) ;
   viewportWidth = parseInt(viewportWidth);
-  var fileWidth = parseInt(fileportWidth - 6);
-  console.log("File Width: " + fileWidth);
-  var statusBtm = ((viewportHeight + toolbarHeight) - (parseInt(statusbar) + 6));
-  var fileHeight = viewportHeight - parseInt(statusbar) - 6;
-  console.log("status is " + statusBtm + " from the top");
-  jQuery('.file_port').css("width", fileportWidth);
-  jQuery('.file_port').css("height", fileHeight + 6);
-  jQuery('div.ui-widget-content').css("height", fileHeight );
-  jQuery('.file').css("width", fileWidth );
-  jQuery('.tool_pane-skel').css("left", fileportWidth  );
-  jQuery('.status_bar').css("top", statusBtm ); // for borders
-  jQuery('.window').css("height", viewportHeight);
-  jQuery('.window').css("width", viewportWidth);
+  var fileWidth = parseInt(fileportWidth - 8);
+  var fileportHeight = viewportHeight - 8;
+  // console.log("File Width: " + fileWidth); // DEBUG
+  var topMargin = 6;
+  var tabRow = $( '#tabrow' ).outerHeight(true);
+  var statusBtm = ((viewportHeight + toolbarHeight) - (parseInt(statusbar) + topMargin));
+  var fileHeight = fileportHeight - (parseInt(statusbar) - tabRow - topMargin);
+  // console.log("status is " + statusBtm + " from the top"); // DEBUG
+  console.log("File height is " + fileHeight );
+  $('.file_port').css("width", fileportWidth);
+  $('.file_port').css("height", fileportHeight);
+  $('.file').css("height", fileHeight );
+  $('.file').css("width", fileWidth );
+  $('.tool_pane-skel').css("left", fileportWidth  );
+  $('.status_bar').css("top", statusBtm ); 
+  $('.window').css("height", viewportHeight);
+  $('.window').css("width", viewportWidth);
 };
 
-jQuery(document).ready(function() {
+$(document).ready(function() {
   checkWidth();
   $(window).resize(checkWidth);
 });
