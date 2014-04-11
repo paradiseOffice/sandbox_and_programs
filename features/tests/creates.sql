5dc8b3c92275f9d583cc129bd89ab0276cccb8cc
Hazel Windle <lead-dev@linux-paradise.co.uk>
2014-04-11 21:41:16 +0100

    restructured dir tree into MVC framework

--- features/tests/creates.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE manTesting (
-       testID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-       app VARCHAR(30) NOT NULL,
-       category VARCHAR(30) NOT NULL,
-       descr TEXT NOT NULL,
-       status ENUM("PASS", "FAIL")
-);
--- local_server_update_R.sh
+++ local_server_update_R.sh
@@ -4,12 +4,12 @@
 # Puts all the paradise office files in
 # The correct local server directories.
 
-cp -R * /var/www/localhost/htdocs/paradise/
+cp -R * /var/www/paradise/
 cp -R home__web_includes/* /home/web_includes/
-chown -R hazel: /home/web_includes/
-chown -R hazel: /home/uploads
+chown -R work: /home/web_includes/
+chown -R work: /home/uploads
 chmod 777 /home/uploads
 chmod -R 644 /home/uploads/*
 chmod -R 755 /home/web_includes/
-chown -R hazel: /var/www/localhost/htdocs/paradise/
-chmod -R 755 /var/www/localhost/htdocs/paradise/ 
+chown -R work: /var/www/paradise/
+chmod -R 755 /var/www/paradise/ 
